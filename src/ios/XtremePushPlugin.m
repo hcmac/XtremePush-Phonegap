29fb20c4a91e6e0d6095278f9c62e6cc9e837258
hcmobileapp <apps.hotcourses@gmail.com>
2019-05-03 15:43:40 +0530

    Update XtremePushPlugin.m

--- src/ios/XtremePushPlugin.m
+++ src/ios/XtremePushPlugin.m
@@ -209,6 +209,7 @@ static NSMutableDictionary *pushNotificationBackupList;
 
 - (void)requestPushPermissions:(CDVInvokedUrlCommand *)command {
     [self.commandDelegate runInBackground:^{
+        [XPush setSandboxModeEnabled:YES];
         [XPush registerForRemoteNotificationTypes:XPNotificationType_Alert | XPNotificationType_Sound | XPNotificationType_Badge];
     }];
 }
